33a93edefa32e9a1f18645d67ce0d3c096ba2aa0
Ralfe Fernando Pereira Ngola <Ralfefernandes@hotmail.com>
2023-05-17 10:40:57 +0200

    Update edit-adressen.blade.php

--- resources/views/update-details/edit-adressen.blade.php
+++ resources/views/update-details/edit-adressen.blade.php
@@ -5,17 +5,28 @@
         <div class="row">
             <h1>Edit Adresgegevens</h1>
             <div class="col-md-3">
+                @foreach ($adressenColumns as $column)
+                    <div class="form-group">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <div>
+                                <label for="{{ $column }}" class="mr-2">{{ ucfirst(str_replace('_', ' ', $column)) }}</label>
+                                <form action="{{ route('update-adressen.update', ['id' => $adres->id]) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <input type="text" class="form-control d-inline-block" id="{{ $column }}" name="{{ $column }}" value="{{ $adres->$column }}" >
+                                </form>
+                            </div>
+                            <form action="{{ route('delete-adressen', ['id' => $adres->id, 'column' => $column]) }}" method="POST" style="display: inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
                 <form action="{{ route('update-adressen.update', ['id' => $adres->id]) }}" method="POST">
                     @csrf
                     @method('PUT')
-                    @foreach ($adressenColumns as $column)
-                        @if ($column !== 'kvk')
-                            <div class="form-group">
-                                <label for="{{ $column }}">{{ ucfirst(str_replace('_', ' ', $column)) }}</label>
-                                <input type="text" class="form-control" id="{{ $column }}" name="{{ $column }}" value="{{ $adres->$column }}" required>
-                            </div>
-                        @endif
-                    @endforeach
                     <button type="submit" class="btn btn-primary">Update</button>
                 </form>
             </div>
