c91bc8cc217dc7507cb654a10f7100d3c26786d2
Ralfe Fernando Pereira Ngola <Ralfefernandes@hotmail.com>
2023-05-17 10:41:18 +0200

    Update edit-bedrijven.blade.php

--- resources/views/update-details/edit-bedrijven.blade.php
+++ resources/views/update-details/edit-bedrijven.blade.php
@@ -4,15 +4,28 @@
     <div class="row">
         <div class="col-md-5">
             <h1>Edit Bedrijven Details</h1>
+            @foreach ($bedrijvenColumns as $column)
+                <div class="form-group">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div>
+                            <label for="{{ $column }}" class="mr-2">{{ ucfirst(str_replace('_', ' ', $column)) }}</label>
+                            <form action="{{ route('update-bedrijven.update', ['id' => $bedrijf->id]) }}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <input type="text" class="form-control d-inline-block" id="{{ $column }}" name="{{ $column }}" value="{{ $bedrijf->$column }}">
+                            </form>
+                        </div>
+                        <form action="{{ route('delete-bedrijven', ['id' => $bedrijf->id, 'column' => $column]) }}" method="POST" style="display: inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
             <form action="{{ route('update-bedrijven.update', ['id' => $bedrijf->id]) }}" method="POST">
                 @csrf
                 @method('PUT')
-                @foreach ($bedrijvenColumns as $column)
-                        <div class="form-group">
-                            <label for="{{ $column }}">{{ ucfirst(str_replace('_', ' ', $column)) }}</label>
-                            <input type="text" class="form-control" id="{{ $column }}" name="{{ $column }}" value="{{ $bedrijf->$column }}" required>
-                        </div>
-                @endforeach
                 <button type="submit" class="btn btn-primary">Update</button>
             </form>
         </div>
