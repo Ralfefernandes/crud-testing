e60eaddeeb1620b1d6ce349a5e108a421d400e81
Ralfe Fernando Pereira Ngola <Ralfefernandes@hotmail.com>
2023-05-17 15:17:02 +0200

    Update edit-adressen.blade.php

--- resources/views/update-details/edit-adressen.blade.php
+++ resources/views/update-details/edit-adressen.blade.php
@@ -6,23 +6,25 @@
             <h1>Edit Adresgegevens</h1>
             <div class="col-md-3">
                 @foreach ($adressenColumns as $column)
-                    <div class="form-group">
-                        <div class="d-flex align-items-center">
-                            <form action="{{ route('update-adressen.update', ['id' => $adres->id]) }}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <label for="{{ $column }}" class="mr-2">{{ ucfirst(str_replace('_', ' ', $column)) }}</label>
-                                <input type="hidden" name="column" value="{{ $column }}">
-                                <input type="text" class="form-control d-inline-block" id="{{ $column }}" name="{{ $column }}" value="{{ $adres->$column }}">
-                                <button type="submit" class="btn btn-primary ml-2">Update</button>
-                            </form>
-                            <form action="{{ route('delete-adressen', ['id' => $adres->id, 'column' => $column]) }}" method="POST" style="display: inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger ml-2">Delete</button>
-                            </form>
+                    @if ($column != 'kvk')
+                        <div class="form-group">
+                            <div class="d-flex align-items-center">
+                                <form action="{{ route('update-adressen.update', ['id' => $adres->id]) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <label for="{{ $column }}" class="mr-2">{{ ucfirst(str_replace('_', ' ', $column)) }}</label>
+                                    <input type="hidden" name="column" value="{{ $column }}">
+                                    <input type="text" class="form-control d-inline-block" id="{{ $column }}" name="{{ $column }}" value="{{ $adres->$column }}">
+                                    <button type="submit" class="btn btn-primary ml-2">Update</button>
+                                </form>
+                                <form action="{{ route('delete-adressen', ['id' => $adres->id, 'column' => $column]) }}" method="POST" style="display: inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger ml-2">Delete</button>
+                                </form>
+                            </div>
                         </div>
-                    </div>
+                    @endif
                 @endforeach
             </div>
         </div>
