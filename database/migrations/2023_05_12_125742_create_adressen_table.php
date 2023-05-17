37a2ca96e06cebfe916d6880e90c735bbcc3ad37
Ralfe Fernando Pereira Ngola <Ralfefernandes@hotmail.com>
2023-05-17 10:38:51 +0200

    Update 2023_05_12_125742_create_adressen_table.php

--- database/migrations/2023_05_12_125742_create_adressen_table.php
+++ database/migrations/2023_05_12_125742_create_adressen_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('adressen', function (Blueprint $table) {
             $table->id();
-            $table->string('beschrijving')->default('');
-            $table->string('straatnaam')->default('');
-            $table->string('huisnummer')->default('');
-            $table->string('postcode')->default('');
-            $table->string('plaatsnaam')->default('');
-            $table->string('land')->default('');
-            $table->string('kvk')->default(''); // Add this line to store the kvk value
+            $table->string('beschrijving')->nullable()->default(null);
+            $table->string('straatnaam')->nullable()->default(null);
+            $table->string('huisnummer')->nullable()->default(null);
+            $table->string('postcode')->nullable()->default(null);
+            $table->string('plaatsnaam')->nullable()->default(null);
+            $table->string('land')->nullable()->default(null);
+            $table->string('kvk')->nullable()->default(null);
             $table->foreign('kvk')->references('kvk')->on('bedrijven'); // Add foreign key constraint
             $table->timestamps();
         });
